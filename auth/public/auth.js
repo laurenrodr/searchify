9f7accacb998d1171fbe9889da44c452a1fc11da
Lauren Rodriguez <laurenrodriguez729@gmail.comm>
2019-09-12 12:59:20 -0400

    update authorization

--- auth/public/auth.js
+++ auth/public/auth.js
@@ -1,8 +1,6 @@
-/*
- * Spotify Authentication
- */
-
 const limit = 10;
+
+var stateKey = 'spotify_auth_state';
 
 /**
  * Obtains parameters from the hash of the URL
@@ -18,34 +16,42 @@ function getHashParams() {
   return hashParams;
 }
 
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+function generateRandomString(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 var userProfileSource = document.getElementById('user-profile-template').innerHTML,
     userProfileTemplate = Handlebars.compile(userProfileSource),
     userProfilePlaceholder = document.getElementById('user-profile');
 
-var oauthSource = document.getElementById('oauth-template').innerHTML,
+    oauthSource = document.getElementById('oauth-template').innerHTML,
     oauthTemplate = Handlebars.compile(oauthSource),
     oauthPlaceholder = document.getElementById('oauth');
 
-var modalSelectSource = document.getElementById('modal-select-template').innerHTML,
-    modalSelectTemplate = Handlebars.compile(modalSelectSource),
-    modalSelectPlaceholder = document.getElementById('modal-select');
-
 var params = getHashParams();
 
-let access_token = params.access_token,
-    refresh_token = params.refresh_token,
-    error = params.error;
+var access_token = params.access_token,
+    state = params.state,
+    storedState = localStorage.getItem(stateKey);
 
-if (error) {
+
+
+if (access_token && (state == null || state !== storedState)) {
   alert('There was an error during the authentication');
 } else {
+  localStorage.removeItem(stateKey);
   if (access_token) {
-    // render oauth info
-    oauthPlaceholder.innerHTML = oauthTemplate({
-      access_token: access_token,
-      refresh_token: refresh_token
-    });
-
     $.ajax({
         url: 'https://api.spotify.com/v1/me',
         headers: {
@@ -53,34 +59,36 @@ if (error) {
         },
         success: function(response) {
           userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-          modalSelectPlaceholder.innerHTML = modalSelectTemplate(response);
 
           $('#login').hide();
           $('#loggedin').show();
         }
     });
   } else {
-      // render initial screen
       $('#login').show();
       $('#loggedin').hide();
   }
 
-  document.getElementById('obtain-new-token').addEventListener('click', function() {
-    $.ajax({
-      url: '/refresh_token',
-      data: {
-        'refresh_token': refresh_token
-      }
-    }).done(function(data) {
-      access_token = data.access_token;
-      oauthPlaceholder.innerHTML = oauthTemplate({
-        access_token: access_token,
-        refresh_token: refresh_token
-      });
-    });
+  document.getElementById('login-button').addEventListener('click', function() {
+
+    var client_id = '3e9119994b24487a83bf048a39832465'; // Your client id
+    var redirect_uri = 'http://localhost:8888'; // Your redirect uri
+
+    var state = generateRandomString(16);
+
+    localStorage.setItem(stateKey, state);
+    var scope = 'user-read-private user-read-email user-top-read';
+
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+
+    window.location = url;
   }, false);
 }
-
 
 function getTopTracks() {
 
